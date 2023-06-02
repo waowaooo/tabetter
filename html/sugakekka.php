5ea56b388daa35cd3dd1e315519e3fb2e9188680
waowaooo <2101010@s.asojuku.ac.jp>
2023-06-02 10:37:28 +0900

    Update sugakekka.php

--- html/sugakekka.php
+++ html/sugakekka.php
@@ -5,14 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-<body>
-<?php 
+    <?php 
     require_once '../DAO/search.php';
     $kekka = new DAO_searc();
 ?>
+</head>
+<body>
+
 <div>
 <?= $kekka->getPostDetail($_POST{'keyword'}); ?>
-</div>
+</div><div >ああああ</div>
 </body>
 </html>
