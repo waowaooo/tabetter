57a212bed7a9c3b82fb8a1d4cb63c701e2e34a49
waowaooo <2101010@s.asojuku.ac.jp>
2023-06-05 09:40:20 +0900

    a

--- html/sugakekka.php
+++ html/sugakekka.php
@@ -5,15 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php 
-    require_once '../DAO/search.php';
-    $kekka = new DAO_searc();
-?>
+<?php
+echo "あああああ";
+?>   
+    
 </head>
 <body>
 
 <div>
-<?= $kekka->getPostDetail($_POST{'keyword'}); ?>
 </div><div >ああああ</div>
 </body>
 </html>
