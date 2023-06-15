7a93442529435d453ddff32a00eb4267c375a851
hanabonbon <107159047+hanabonbon@users.noreply.github.com>
2023-06-15 15:15:09 +0900

    ランク機能完成

--- DAO/likesdb.php
+++ DAO/likesdb.php
@@ -1,17 +1,37 @@
 <?php
+class DAO_likes{
+//データベースに接続する関数
+private function dbConnect(){
+        //データベースに接続
+        $pdo = new PDO('mysql:host=localhost; dbname=tabetterdb; charset=utf8',
+                        'webuser', 'abccsd2');
+        return $pdo;
+}
 
-//データベースに接続
-$pdo = new PDO('mysql:host=localhost; dbname=tabetterdb; charset=utf8',
-                'webuser', 'abccsd2');
+        //フォーラムIDを指定して表示する関数
+        public function addLikes($forumid){
+                $pdo = $this -> dbConnect();
+                $id = intval($postid);  //$idをstring型からint型に変換
+                $sql = "INSERT INTO likes(post_id,user_id)
+                                VALUES(?,?)";
+                $ps = $pdo->prepare($sql);
+                $ps -> bindValue(1$id,PDO::PARAM_INT);
+                $ps -> bindValue(2,$userid,PDO::PARAM_STR);
+                //SQLの実行
+                $ps->execute();
+        }
 
+        public function deleteLikes($postid,$userid){
+                $pdo = $this -> dbConnect();
+                $id = intval($postid);  //$idをstring型からint型に変換
+                $sql = "DELETE FROM likes WHERE post_id = ? AND user_id = ?";
+                $ps = $pdo->prepare($sql);
+                $ps -> bindValue(1$id,PDO::PARAM_INT);
+                $ps -> bindValue(2,$userid,PDO::PARAM_STR);
+                //SQLの実行
+                $ps->execute();
+        }
 
-$id = intval($_POST['id']);  //$idをstring型からint型に変換
-$sql = "INSERT INTO likes(post_id,user_id)
-        VALUES(?,?)";
-$ps = $pdo->prepare($sql);
-$ps -> bindValue(1$id,PDO::PARAM_INT);
-$ps -> bindValue(2,$_POST['userId'],PDO::PARAM_STR);
-//SQLの実行
-$ps->execute();
+}
 
 ?>
