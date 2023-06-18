f39bfe74b80f75a495a0f60bb1c4102e51a48187
hanabonbon <107159047+hanabonbon@users.noreply.github.com>
2023-06-18 18:04:27 +0900

    いけました

--- html/myProfile2.php
+++ html/myProfile2.php
@@ -29,7 +29,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/Bar3.css">
-    <link rel="stylesheet" href="../css/Oyamadaprofile.css">
+    <link rel="stylesheet" href="../css/profile2.css">
     <link rel="stylesheet" href="../css/modal.css">
     <link rel="stylesheet" href="../css/scrollable.css">
 </head>
